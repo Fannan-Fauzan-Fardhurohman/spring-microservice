3542422b4c9579a1dda3a8874278662fda1078ad
unknown <fannanff25@gmail.com>
2024-08-03 22:47:16 +0700

    feat: maintenance circuit breaker

--- order-service/src/main/java/com/fanxan/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/fanxan/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -48,6 +48,7 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            return "Order Placed Successsfully";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
